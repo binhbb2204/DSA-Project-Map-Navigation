d092508338b107c9f5542d94e619ae821879e8ba
Thái Quang Tính <tinhthai2000@gmail.com>
2024-05-26 09:59:00 +0700

    revise folders

--- Map/src/waypoint/ButtonWaypoint.java
+++ Map/src/waypoint/ButtonWaypoint.java
@@ -6,10 +6,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class ButtonWaypoint extends JButton {
+
     public ButtonWaypoint() {
         setContentAreaFilled(false);
-        setIcon(new ImageIcon(getClass().getResource("/icon/waypoint.png")));
+        setIcon(new ImageIcon(getClass().getResource("/icon/pin.png")));
         setCursor(new Cursor(Cursor.HAND_CURSOR));
-        setSize(new Dimension(70, 70));
+        setSize(new Dimension(24, 24));
     }
 }
--- Map/src/waypoint/MyWaypoint.java
+++ Map/src/waypoint/MyWaypoint.java
@@ -7,57 +7,42 @@ import org.jxmapviewer.viewer.DefaultWaypoint;
 import org.jxmapviewer.viewer.GeoPosition;
 
 public class MyWaypoint extends DefaultWaypoint {
-    private String name;
-    private JButton button;
-    private PointType pointType;
 
-    public PointType getPointType() {
-        return pointType;
-    }
-
-    public void setPointType(PointType pointType) {
-        this.pointType = pointType;
-    }
-
-    public MyWaypoint(String name, PointType pointType, EventWaypoint event, GeoPosition coord) {
-        super(coord);
-        this.name = name;
-        initButton(event);
-        this.pointType = pointType;
-    }
-   
     public String getName() {
         return name;
-    }
-
-    public JButton getButton() {
-        return button;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public JButton getButton() {
+        return button;
+    }
+
     public void setButton(JButton button) {
         this.button = button;
     }
-    
-    public MyWaypoint() {
-        
+
+    public MyWaypoint(String name, EventWaypoint event, GeoPosition coord) {
+        super(coord);
+        this.name = name;
+        initButton(event);
     }
-    
+
+    public MyWaypoint() {
+    }
+
+    private String name;
+    private JButton button;
+
     private void initButton(EventWaypoint event) {
         button = new ButtonWaypoint();
         button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent ae) {
                 event.selected(MyWaypoint.this);
             }
         });
     }
-    
-    public static enum PointType {
-        START,END
-    }
-  
 }
