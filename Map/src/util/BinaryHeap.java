5e0fceae5935cbb64e060ddec8f0aa035517c988
Thái Quang Tính <tinhthai2000@gmail.com>
2024-06-17 09:34:09 +0700

    clean code

--- Map/src/util/BinaryHeap.java
+++ Map/src/util/BinaryHeap.java
@@ -1,12 +1,3 @@
-/*
- * Author:       Thái Quang Tính
- * Created on    17-06-2024
- * GitHub:       https://github.com/notrealqt
- * Email:        tinhthai2000@gmail.com
- * Description:  [Brief description of the file or project]
-*/
-
-
 package util;
 
 import java.util.*;
